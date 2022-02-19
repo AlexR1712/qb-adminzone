1541568a1c29b8bc7a83ba052b894662753decca
Alexander J. Rodriguez D <alexr1712@gmail.com>
2022-02-18 20:53:56 -0400

    Update resource manifest

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'Nick Perry'
+description 'Admin Zone'
+version '1.0.4'
+
+shared_script 'config.lua'
+client_script 'client.lua'
+server_script 'server.lua'
